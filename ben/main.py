1fa6873de88b674b5bb276254fc48aec05e2bf20
benw10-1 <benjaminwirth10@gmail.com>
2021-06-25 19:25:10 -0700

    Update main.py

--- ben/main.py
+++ ben/main.py
@@ -1,7 +1,5 @@
 import os
-import pickle
 import queue
-import sys
 import bisect
 import shelve
 import threading
@@ -33,8 +31,8 @@ class Basic(StructuredNode):
     link = StringProperty(required=False)
 
 
-def print_decompressed():
-    print({zlib.decompress(key).decode("utf-8"): [zlib.decompress(x).decode("utf-8") for x in table[key]] for key in table})
+def print_decompressed(d):
+    print({zlib.decompress(key).decode("utf-8"): [zlib.decompress(x).decode("utf-8") for x in d[key]] for key in d})
 
 
 def update_4j():
@@ -63,41 +61,51 @@ def process_wiki(site, depth):
     global q, table
     if depth < 1:
         return
+    
+    # check if site is in compressed form or not
     if type(site) != str:
         site = zlib.decompress(site).decode("utf-8")
-
+    
+    # request information
     page = requests.get(base + site)
     tree = html.fromstring(page.content)
 
     store = []
-
+    
+    # iterate through all matched elements
     for el in tree.xpath(xpath):
         try:
             el.attrib['href']
         except KeyError:
             continue
-
+        # cleans up link
+        if el.attrib['href'].count("/") > 2:
+            el.attrib['href'] = "/".join(link.split("/")[:3])
         if bin_search(links, site) != -1:
             if "/wiki/" == el.attrib['href'][:6] and ("BookSources" and "citation_needed" and "Citation_needed" and ":") \
                     not in el.attrib['href'] and el.text_content():
-                if el.attrib['href'].count("/") > 2:
-                    el.attrib['href'] = "/".join(link.split("/")[:3])
+                               
                 # compresses data to be decompressed and read later
                 bytes_href = bytes(el.attrib['href'], "utf-8")
                 compressed = zlib.compress(bytes_href)
-
+                
+                # add to temp array the links being processed
                 store.append(compressed)
+                # add to queue the next link to be processed
                 q.put((el.attrib['href'], depth - 1))
-
+    
+    # write to table with the compressed key and compressed text within array
     table[zlib.compress(bytes(site, "utf-8"))] = store
-
+    
+    # insert sorted into links for easier and more efficient searching down the line
     bisect.insort(site)
 
 
 if __name__ == '__main__':
     base = "https://en.wikipedia.org"
     link = "/wiki/Jarvis_Island/People"
-
+    
+    # global queue
     q = queue.Queue()
 
     table = {}
@@ -107,18 +115,14 @@ if __name__ == '__main__':
     index = 0
 
     storage_file = "new_store.pick"
-
+    
+    # checks for storage file and loads storage file
     if os.path.exists(storage_file) and os.path.getsize(storage_file) > 0:
         with shelve.open(storage_file) as file:
             links = file['processed']
             index = file['index']
             file.close()
 
-    # to decompress and print out the pickled dictionary file
-    """print_decompressed()
-    print(sys.getsizeof(table))
-    print(len(table))
-    exit()"""
     # includes "see also" part
     xpath = '//div[@id="mw-content-text"]//p//a'
 
@@ -136,15 +140,17 @@ if __name__ == '__main__':
 
     # LOOP FOR CHECKING WHEN IT IS DONE AND ALSO WRITING to pickle file
     while True:
+        index += 1
         time.sleep(5)
+        # check if table has not changed since last cycle
         if not table:
             not_done = False
             break
+        # writes to storage file
         with shelve.open(storage_file) as file:
             file['processed'] = links
             file[str(index)] = table
+            file['index'] = index
             file.close()
-
-        index += 1
 
         table = {}
