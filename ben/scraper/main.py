5d79175c2e94943c75d0be14855c6d9fdd9bffaa
benw10-1 <benjaminwirth10@gmail.com>
2021-07-12 11:21:00 -0700

    Update main.py

--- ben/scraper/main.py
+++ ben/scraper/main.py
@@ -1,13 +1,8 @@
-import os
+from lxml import html
+import requests
 import queue
-import bisect
-import shelve
 import threading
 import time
-import zlib
-
-import requests
-from lxml import html
 from neomodel import StructuredNode, StringProperty, Relationship, config
 
 
@@ -31,131 +26,75 @@ class Basic(StructuredNode):
     link = StringProperty(required=False)
 
 
-def print_decompressed(d):
-    print({zlib.decompress(key).decode("utf-8"): [zlib.decompress(x).decode("utf-8") for x in d[key]] for key in d})
-
-
-def update_4j(d):
-    user = "neo4j"
-    password = "1234"
-
-    server = "bolt://{}:{}@localhost:7687".format(user, password)
-    config.DATABASE_URL = server
-
-    for x in Basic.nodes.all():
-        print(x)
-        x.delete()
-
-
 def worker():
-    global q, not_done
-    try:
-        while not_done:
-            link_, depth_ = q.get(block=True, timeout=5)
-            process_wiki(link_, depth_)
-    except queue.Empty:
-        print(links)
-        return
+    while True:
+        title, link_, depth, node_ = q.get()
+        process_wiki(title, link_, depth, node_)
 
 
-def process_wiki(site, depth):
-    global q, table
+def process_wiki(title, site, depth, parent):
+    global q
+    this_node = Basic(name=title, link=site)
+    this_node.save()
+    if parent:
+        this_node.to.connect(parent)
+
     if depth < 1:
         return
 
-    # check if site is in compressed form or not
-    if type(site) != str:
-        site = zlib.decompress(site).decode("utf-8")
-
-    # request information
     page = requests.get(base + site)
     tree = html.fromstring(page.content)
 
-    store = []
-
-    # iterate through all matched elements
     for el in tree.xpath(xpath):
         try:
             el.attrib['href']
         except KeyError:
             continue
-        # cleans up link
-        if el.attrib['href'].count("/") > 2:
-            el.attrib['href'] = "/".join(link.split("/")[:3])
-        if bin_search(links, site) == -1:
-            if "/wiki/" == el.attrib['href'][:6] and ("BookSources" and "citation_needed" and "Citation_needed" and ":") \
-                    not in el.attrib['href'] and el.text_content():
 
-                # compresses data to be decompressed and read later
-                bytes_href = bytes(el.attrib['href'], "utf-8")
-                compressed = zlib.compress(bytes_href)
+        search = Basic.nodes.get_or_none(link=el.attrib['href'])
 
-                # add to temp array the links being processed
-                store.append(compressed)
-                # add to queue the next link to be processed
-                q.put((el.attrib['href'], depth - 1))
-
-    # write to table with the compressed key and compressed text within array
-    table[zlib.compress(bytes(site, "utf-8"))] = store
-
-    # insert sorted into links for easier and more efficient searching down the line
-    bisect.insort(links, site)
+        if "/wiki/" == el.attrib['href'][:6] and ("BookSources" and "citation_needed" and "Citation_needed" and ":") \
+                not in el.attrib['href'] and el.text_content():
+            if search:
+                this_node.to.connect(search)
+            else:
+                q.put((el.text_content(), el.attrib['href'], depth - 1, this_node))
 
 
 if __name__ == '__main__':
+    server = "bolt://neo4j:1234@localhost:7687"
+    config.DATABASE_URL = server
+
+    user = "neo4j"
+    password = "1234"
+
+    for x in Basic.nodes.all():
+        print(x)
+        x.delete()
+
     base = "https://en.wikipedia.org"
     link = "/wiki/Jarvis_Island/People"
 
-    # global queue
-    q = queue.Queue()
-
-    table = {}
-
-    # seen links overall since emptying the dict to save memory
-    links = []
-    index = 0
-
-    storage_file = "new_store"
-
-    # checks for storage file and loads storage file
-    try:
-        with shelve.open(storage_file) as file:
-            links = file['processed']
-            index = file['index']
-            file.close()
-    except KeyError:
-        print("New file created")
-
-    print(links)
-
-    # includes "see also" part
-    xpath = '//div[@id="mw-content-text"]//p//a'
-
-    # clean up link
     if link.count("/") > 2:
         link = "/".join(link.split("/")[:3])
 
-    # create threads and establish thread "signal" variable
-    not_done = True
+    # with "see also part"
+    xpath = '//div[@id="mw-content-text"]//p//a'
+
+    q = queue.Queue()
+    threads = []
+
     for _ in range(10):
         t = threading.Thread(target=worker)
         t.start()
+        threads.append(t)
 
-    process_wiki(link, 2)
+    process_wiki(link.split("/")[2], link, 2, None)
 
-    # LOOP FOR CHECKING WHEN IT IS DONE AND ALSO WRITING to pickle file
+    old = None
     while True:
-        index += 1
-        time.sleep(5)
-        # check if table has not changed since last cycle
-        if not table:
-            not_done = False
-            break
-        # writes to storage file
-        with shelve.open(storage_file) as file:
-            file['processed'] = links
-            file[str(index)] = table
-            file['index'] = index
-            file.close()
-
-        table = {}
+        time.sleep(10)
+        length = len(Basic.nodes.all())
+        if length == old:
+            exit()
+        old = length
